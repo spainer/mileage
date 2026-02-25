b8acb3e6999cc8d9c86e37e9bf304b490b30b077
Sven Painer <sven@painer.de>
2026-02-25 20:24:16 +0100

    Initial commit.

---
+++ src/test/java/de/painer/mileage/MileageApplicationTests.java
@@ -0,0 +1,13 @@
+package de.painer.mileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
