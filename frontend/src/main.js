e74f6dc3b7997f882e24640efb4f915415283c2d
Sven Painer <sven@painer.de>
2026-03-06 20:51:59 +0100

    created frontend project

---
+++ frontend/src/main.js
@@ -0,0 +1,26 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import Aura from '@primeuix/themes/aura'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+
+app.component('Button', Button)
+
+app.mount('#app')
